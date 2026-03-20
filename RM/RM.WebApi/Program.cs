e2889d7fac2fcdef8b5383a3960054c790ddfde7
Anton Sukhov <RealComrade2011@yandex.ru>
2026-03-20 18:34:26 +0300

    Миграция на новый подход настройки хоста (Minimal Hosting)

--- RM/RM.WebApi/Program.cs
+++ RM/RM.WebApi/Program.cs
@@ -1,47 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using RM.WebApi;
 
-namespace RM.WebApi;
+var builder = WebApplication.CreateBuilder(args);
 
-/// <summary>
-/// Основной класс приложения ASP.NET Core Web API.
-/// </summary>
-/// <remarks>
-/// Содержит точку входа в приложение (метод <see cref="Main"/>) и логику создания хоста.
-/// <see cref="Program"/> связан с настройкой инфраструктуры приложения, которая меняется редко. 
-/// </remarks>
-public class Program
-{
-    /// <summary>
-    /// Инициализирует экземпляр <see cref="Program"/>.
-    /// </summary>
-    protected Program() { }
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
-    /// <summary>
-    /// Запускает веб‑приложение, создавая и запуская хост с настройками по умолчанию.
-    /// </summary>
-    /// <remarks>Точка входа в приложение ASP.NET Core.</remarks>
-    /// <param name="args">Массив аргументов командной строки, переданных при запуске приложения.</param>
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args)
-            .Build()    
-            .Run();     
-    }
+var app = builder.Build();
 
-    /// <summary>
-    /// Создаёт и настраивает построитель хоста (<see cref="IHostBuilder"/>) для веб‑приложения.
-    /// </summary>
-    /// <remarks>Использует стандартные настройки ASP.NET Core и подключает класс <see cref="Startup"/>
-    /// для конфигурации сервисов и конвейера обработки запросов.</remarks>
-    /// <param name="args">Аргументы командной строки.</param>
-    /// <returns>Настроенный экземпляр <see cref="IHostBuilder"/>, готовый к созданию хоста.</returns>
-    private static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                // Указывает использовать класс Startup для настройки приложения:
-                // регистрации сервисов в DI‑контейнере и настройки конвейера middleware
-                webBuilder.UseStartup<Startup>();
-            });
-}
+startup.Configure(app);
+
+app.Run();
--- RM/RM.WebApi/Startup.cs
+++ RM/RM.WebApi/Startup.cs
@@ -22,6 +22,11 @@ namespace RM.WebApi
     public class Startup
     {
         /// <summary>
+        /// Получает настройки приложения.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
         /// Инициализирует экземпляр <see cref="Startup"/>.
         /// </summary>
         /// <param name="configuration">Настройки приложения.</param>
@@ -31,17 +36,11 @@ namespace RM.WebApi
         }
 
         /// <summary>
-        /// Получает настройки приложения.
-        /// </summary>
-        public IConfiguration Configuration { get; }
-
-        /// <summary>
         /// Выполняет регистрацию сервисов, используемых в приложении.
         /// </summary>
         /// <param name="services">Коллекция сервисов.</param>
         public void ConfigureServices(IServiceCollection services)
-        {
-            
+        {           
             services.RegisterDbContexts(Configuration);
             services.RegisterSettings(Configuration);
             services.RegisterRepositories();         
@@ -61,6 +60,7 @@ namespace RM.WebApi
                     $"{Assembly.GetExecutingAssembly().GetName().Name}.xml", // XML текущего проекта
                     "RM.BLL.Abstractions.xml",                               // XML первого связанного проекта
                 };
+
                 foreach (var xmlFile in xmlFiles.Select(file => Path.Combine(AppContext.BaseDirectory, file))
                                                 .Where(xmlFile => File.Exists(xmlFile)))
                 {
@@ -72,39 +72,24 @@ namespace RM.WebApi
         /// <summary>
         /// Выполняет настройку конвейера HTTP-запросов. Вызывается средой выполнения.
         /// </summary>
-        /// <param name="app">Создатель компонентов конвейера HTTP-запросов.</param>
-        /// <param name="env">Информация о среде веб-хостинга, в которой работает приложение.</param>
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /// <param name="app">Веб-приложение.</param>
+        public void Configure(WebApplication app)
         {        
-            if (env.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
                 app.UseSwagger();
-
-                // Запускает веб‑интерфейс Swagger UI для интерактивного тестирования API.
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RM.WebApi v1"));
             }
 
-            // Глобальный обработчик исключений
             app.UseMiddleware<ErrorHandlingMiddleware>();
-
-            // Перенаправляет HTTP‑запросы на HTTPS.
             app.UseHttpsRedirection();
-
-            //Для обслуживания CSS/JS
             app.UseStaticFiles();
-
-            // Настраивает маршрутизацию — определяет, какой код будет вызван для каждого URL
             app.UseRouting();
-
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapRazorPages();
-            });
+            app.MapControllers();
+            app.MapRazorPages();
         }
     }
 }
