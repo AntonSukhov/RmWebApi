48f622324732c7dc81cec139888d94185ebd7109
Anton Sukhov <RealComrade2011@yandex.ru>
2026-03-29 12:04:55 +0300

    Обновлен код класса Startup.  Доработан код класса Program.

--- RM/RM.WebApi/Program.cs
+++ RM/RM.WebApi/Program.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using RM.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration, builder.Environment);
 
-var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
--- RM/RM.WebApi/Startup.cs
+++ RM/RM.WebApi/Startup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using IdentityWebApp.Api.Extensions;
+using Infrastructure.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,86 +13,83 @@ using Microsoft.OpenApi;
 using RM.WebApi.Extensions;
 using RM.WebApi.Middleware;
 
-namespace RM.WebApi
+namespace RM.WebApi;
+
+/// <summary>
+/// Компонент начальной конфигурации приложения ASP.NET Core.
+/// </summary>
+/// <remarks>
+/// Отвечает за настройку сервисов (DI‑контейнер) и конвейера обработки HTTP‑запросов.
+/// </remarks>
+internal class Startup: Infrastructure.AspNetCore.StartupBase
 {
     /// <summary>
-    /// Класс начальной конфигурации приложения ASP.NET Core.
+    /// Инициализирует экземпляр <see cref="Startup"/>.
     /// </summary>
-    /// <remarks>
-    /// Отвечает за настройку сервисов (DI‑контейнер) и конвейера обработки HTTP‑запросов.
-    /// </remarks>
-    public class Startup
-    {
-        /// <summary>
-        /// Получает настройки приложения.
-        /// </summary>
-        public IConfiguration Configuration { get; }
+    /// <param name="configuration">Объект конфигурации приложения.</param>
+    /// <param name="environment">Объект окружения приложения.</param>
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment):
+        base(configuration, environment) {}
 
-        /// <summary>
-        /// Инициализирует экземпляр <see cref="Startup"/>.
-        /// </summary>
-        /// <param name="configuration">Настройки приложения.</param>
-        public Startup(IConfiguration configuration)
+    /// <summary>
+    /// Выполняет регистрацию сервисов, используемых в приложении.
+    /// </summary>
+    /// <param name="services">Коллекция сервисов.</param>
+    public override void ConfigureServices(IServiceCollection services)
+    {      
+        base.ConfigureServices(services);
+
+        services.RegisterDbContexts(Configuration);
+        services.RegisterSettings(Configuration);
+        services.RegisterRepositories(); 
+        services.AddIdentityWebAppAuthentication();        
+        services.RegisterServices();
+        services.RegisterValidators();
+        services.RegisterMappingProfiles();
+        
+        services.AddControllers();
+        services.AddRazorPages(); 
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
-        }
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "RM.WebApi", Version = "v1" });
 
-        /// <summary>
-        /// Выполняет регистрацию сервисов, используемых в приложении.
-        /// </summary>
-        /// <param name="services">Коллекция сервисов.</param>
-        public void ConfigureServices(IServiceCollection services)
-        {           
-            services.RegisterDbContexts(Configuration);
-            services.RegisterSettings(Configuration);
-            services.RegisterRepositories(); 
-            services.AddIdentityWebAppAuthentication();        
-            services.RegisterServices();
-            services.RegisterValidators();
-            services.RegisterMappingProfiles();
-           
-            services.AddControllers();
-            services.AddRazorPages(); 
-            services.AddSwaggerGen(c =>
+            // Укажите пути к XML-файлам с комментариями
+            var xmlFiles = new[]
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RM.WebApi", Version = "v1" });
+                $"{Assembly.GetExecutingAssembly().GetName().Name}.xml", // XML текущего проекта
+                "RM.BLL.Abstractions.xml",                               // XML первого связанного проекта
+            };
 
-                // Укажите пути к XML-файлам с комментариями
-                var xmlFiles = new[]
-                {
-                    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml", // XML текущего проекта
-                    "RM.BLL.Abstractions.xml",                               // XML первого связанного проекта
-                };
-
-                foreach (var xmlFile in xmlFiles.Select(file => Path.Combine(AppContext.BaseDirectory, file))
-                                                .Where(xmlFile => File.Exists(xmlFile)))
-                {
-                    c.IncludeXmlComments(xmlFile);
-                }
-            });
-        }
-
-        /// <summary>
-        /// Выполняет настройку конвейера HTTP-запросов. Вызывается средой выполнения.
-        /// </summary>
-        /// <param name="app">Веб-приложение.</param>
-        public void Configure(WebApplication app)
-        {        
-            if (app.Environment.IsDevelopment())
+            foreach (var xmlFile in xmlFiles.Select(file => Path.Combine(AppContext.BaseDirectory, file))
+                                            .Where(xmlFile => File.Exists(xmlFile)))
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RM.WebApi v1"));
+                c.IncludeXmlComments(xmlFile);
             }
+        });
+    }
 
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
+    /// <summary>
+    /// Выполняет настройку конвейера HTTP-запросов. Вызывается средой выполнения.
+    /// </summary>
+    /// <param name="app">Веб-приложение.</param>
+    public override void Configure(WebApplication app)
+    {       
+        base.Configure(app);
 
-            app.MapControllers();
-            app.MapRazorPages();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RM.WebApi v1"));
         }
+
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.MapRazorPages();
     }
 }
