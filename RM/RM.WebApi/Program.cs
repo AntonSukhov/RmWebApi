aa10a2b8be879c829668fac81e31a90c206f9896
Anton Sukhov <RealComrade2011@yandex.ru>
2026-04-12 16:45:12 +0300

    Актуализировал файлы launchSettings.json и appsettings.json. Добавил файл appsettings.Production.json.

--- RM/RM.WebApi/Program.cs
+++ RM/RM.WebApi/Program.cs
@@ -1,7 +1,15 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using RM.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 var startup = new Startup(builder.Configuration, builder.Environment);
 
 startup.ConfigureServices(builder.Services);
